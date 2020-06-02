a5cee190005eb0de64eb035adc1afe0d8592ab13
vaidehi <vaidehi.deshpande05@gmail.com>
2020-06-02 16:55:50 +0530

    new file added e commerce

---
+++ Functional Automation/git_demo_file.py
@@ -0,0 +1,13 @@
+import self as self
+from selenium import webdriver
+import time
+
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
+driver = webdriver.Chrome("D:\Python_Selenium_Concepts\drivers\chromedriver.exe")
+driver.get("https://www.amazon.in/")
+print("e commerce ")
+driver.close()
